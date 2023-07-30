c96452cbc829e5ad834d83c9c582347493e3cdcc
Aryan-401 <aryan.raj.garg@gmail.com>
2023-07-30 17:25:10 +0530

    enabled exit

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import pygame
 
 screen = pygame.display.set_mode((640, 480))
 
-while True:
-    pass
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
