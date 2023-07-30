a26c920e60a807138c8832329053de54b6ae9723
Aryan-401 <aryan.raj.garg@gmail.com>
2023-07-30 17:26:18 +0530

    changed title and favicon

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import pygame
 
 screen = pygame.display.set_mode((640, 480))
 
+# title and icon
+pygame.display.set_caption('Space Invaders')
+icon = pygame.image.load('assets/spaceships/entourage.png')
+pygame.display.set_icon(icon)
+
 running = True
 while running:
     for event in pygame.event.get():
