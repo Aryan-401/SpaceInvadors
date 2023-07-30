4a894843a755b01f15ddad7a3f042d89ad829ec2
Aryan-401 <aryan.raj.garg@gmail.com>
2023-07-30 23:23:57 +0530

    added enemy

--- main.py
+++ main.py
@@ -21,12 +21,21 @@ enemyImg = pygame.image.load(f'assets/enemy/{enemy}')
 print(f"{player_name} vs. {enemy_name}")
 
 
-def draw_player(playerX: int, playerY: int):
-    screen.blit(playerImg, (playerX, playerY))
+def draw_player(playerXloc: float, playerYloc: float):
+    screen.blit(playerImg, (playerXloc, playerYloc))
 
+
+def draw_enemy(enemyXloc: float, enemyYloc: float):
+    screen.blit(enemyImg, (enemyXloc, enemyYloc))
+
+
+enemyX = random.randint(0, 650)
+enemyY = -20
 
 playerX = 370
 playerY = 480
+playerX_change = 0
+playerY_change = 0
 
 running = True
 while running:
@@ -35,5 +44,34 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            elif event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+            elif event.key == pygame.K_UP:
+                playerY_change = -0.3
+            elif event.key == pygame.K_DOWN:
+                playerY_change = 0.3
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerY_change = 0
+    enemyY += 0.1
+    draw_enemy(enemyX, enemyY)
+    if playerX <= 30:
+        playerX = 30
+    elif playerX >= 700:
+        playerX = 700
+    if playerY <= 30:
+        playerY = 30
+    elif playerY >= 500:
+        playerY = 500
+    playerX += playerX_change
+    playerY += playerY_change
     draw_player(playerX, playerY)
+    draw_enemy(enemyX, enemyY)
     pygame.display.update()
